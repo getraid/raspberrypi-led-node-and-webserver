f08d5a35ed7f52b8adc8f035e9d802630e97773e
getraid <32432718+getraid@users.noreply.github.com>
2023-01-25 00:18:41 +0100

    Added config for dev mode, formatted code

--- ws-client.js
+++ ws-client.js
@@ -1,114 +1,97 @@
 var ip = location.host;
-var ws = new WebSocket("ws://"+ip+":3000");
+var ws = new WebSocket("ws://" + ip + ":3000");
 
- var val = [0,0,0];
- 
-  ws.onopen= function(){
-	 setTitle("Connected to Server");
- };
- 
-  ws.onclose = function(){
-	 setTitle("Connection closed, trying to reconnect...");
-	 window.setTimeout(function(){location.reload()},3000);
- };
- ws.onmessage = function(payload) {
-	printMessage(payload.data);
- };
-  
- 
- function pSend(r,g,b){
-	 
-	 var input1 = r
-	 var input2 = g
-	 var input3 = b
-	
-	val[0] = parseInt(input1.value);
-	val[1] = parseInt(input2.value);
-	val[2] = parseInt(input3.value);
-	
-	if(val[0].isNaN ||val[1].isNaN ||val[2].isNaN )
-	{
-	  notInt();
-	}
-	else
-	{
-	 var obj = {"r":val[0],"g":val[1],"b":val[2]};
+var val = [0, 0, 0];
 
-	 //client-side send
-	 ws.send(JSON.stringify(obj));
-	 }
- }
- 
-
- 
- document.forms[0].onsubmit=function(){
-
-	 var input1 = document.getElementById('message1');
-	 var input2 = document.getElementById('message2');
-	 var input3 = document.getElementById('message3');
-	
-pSend(input1,input2 ,input3 );
-
- };
- 
- function setTitle(title){
-	var notify = document.getElementById('conninf');
-	notify.innerHTML = title;
- }
- 
-//not necessary;create div in html with class messages to display
- function printMessage(message){
-	 var p = document.createElement('p');
-	 p.innerText = message;
-	 document.querySelector('div.messages').appendChild(p);
-	 
- }
- 
- 
- //connection close fix;necessary to prevent errors - still works without
- window.onbeforeunload = function() {
-    ws.onclose = function () {}; // disable onclose handler first
-    ws.close()
+ws.onopen = function () {
+  setTitle("Connected to Server");
 };
- 
- //shortcuts
-  function sendBlack()
- {
-	 var obj = {"r":0,"g":0,"b":0}; 
-	 ws.send(JSON.stringify(obj));
- }
-  function sendRed()
- {
-	 var obj = {"r":255,"g":0,"b":0}; 
-	 ws.send(JSON.stringify(obj));
- }
-  function sendGreen()
- {
-	 var obj = {"r":0,"g":255,"b":0}; 
-	 ws.send(JSON.stringify(obj));
- }
-  function sendBlue()
- {
-	 var obj = {"r":0,"g":0,"b":255}; 
-	 ws.send(JSON.stringify(obj));
- }
-   function sendWhite()
- {
-	 var obj = {"r":255,"g":255,"b":255}; 
-	 ws.send(JSON.stringify(obj));
- }  
- function sendYellow()
- {
-	 var obj = {"r":255,"g":255,"b":0}; 
-	 ws.send(JSON.stringify(obj));
- }
-    function sendBBlue()
- {
-	 var obj = {"r":0,"g":255,"b":255}; 
-	 ws.send(JSON.stringify(obj));
- }  
-   function sendPurple()
- {
-	 var obj = {"r":255,"g":0,"b":255}; 
-	 ws.send(JSON.stringify(obj));
- }  
+
+ws.onclose = function () {
+  setTitle("Connection closed, trying to reconnect...");
+  window.setTimeout(function () {
+    location.reload();
+  }, 3000);
+};
+ws.onmessage = function (payload) {
+  printMessage(payload.data);
+};
+
+function pSend(r, g, b) {
+  var input1 = r;
+  var input2 = g;
+  var input3 = b;
+
+  val[0] = parseInt(input1.value);
+  val[1] = parseInt(input2.value);
+  val[2] = parseInt(input3.value);
+
+  if (val[0].isNaN || val[1].isNaN || val[2].isNaN) {
+    notInt();
+  } else {
+    var obj = { r: val[0], g: val[1], b: val[2] };
+
+    //client-side send
+    ws.send(JSON.stringify(obj));
+  }
+}
+
+document.forms[0].onsubmit = function () {
+  var input1 = document.getElementById("message1");
+  var input2 = document.getElementById("message2");
+  var input3 = document.getElementById("message3");
+
+  pSend(input1, input2, input3);
+};
+
+function setTitle(title) {
+  var notify = document.getElementById("conninf");
+  notify.innerHTML = title;
+}
+
+//not necessary;create div in html with class messages to display
+function printMessage(message) {
+  var p = document.createElement("p");
+  p.innerText = message;
+  document.querySelector("div.messages").appendChild(p);
+}
+
+//connection close fix;necessary to prevent errors - still works without
+window.onbeforeunload = function () {
+  ws.onclose = function () {}; // disable onclose handler first
+  ws.close();
+};
+
+//shortcuts
+function sendBlack() {
+  var obj = { r: 0, g: 0, b: 0 };
+  ws.send(JSON.stringify(obj));
+}
+function sendRed() {
+  var obj = { r: 255, g: 0, b: 0 };
+  ws.send(JSON.stringify(obj));
+}
+function sendGreen() {
+  var obj = { r: 0, g: 255, b: 0 };
+  ws.send(JSON.stringify(obj));
+}
+function sendBlue() {
+  var obj = { r: 0, g: 0, b: 255 };
+  ws.send(JSON.stringify(obj));
+}
+function sendWhite() {
+  var obj = { r: 255, g: 255, b: 255 };
+  ws.send(JSON.stringify(obj));
+}
+function sendYellow() {
+  var obj = { r: 255, g: 255, b: 0 };
+  ws.send(JSON.stringify(obj));
+}
+function sendBBlue() {
+  var obj = { r: 0, g: 255, b: 255 };
+  ws.send(JSON.stringify(obj));
+}
+function sendPurple() {
+  var obj = { r: 255, g: 0, b: 255 };
+  ws.send(JSON.stringify(obj));
+}
--- ws.js
+++ ws.js
@@ -1,39 +1,54 @@
+const ConfigParser = require("configparser");
+const fs = require("fs");
+
+const config = new ConfigParser();
+if (fs.existsSync("config.ini")) {
+  config.read("config.ini");
+} else {
+  // no config file found, generate defaults
+  config.addSection("Config");
+  config.set("Config", "LED_GPIO_PIN_RED", 27);
+  config.set("Config", "LED_GPIO_PIN_RED", 22);
+  config.set("Config", "LED_GPIO_PIN_BLUE", 17);
+  config.set("Config", "VerboseLogging", false);
+  config.set("Config", "DebugWithoutGPIO", false);
+  config.write("config.ini");
+  console.warn("Wrote default settings to config.ini. \nPlease change/verify them and relaunch application");
+  process.exit();
+}
+
 //ports
 var wssPort = 3000;
 var websPort = 80;
 
-//pigpio
-var Gpio = require("pigpio").Gpio;
+var x = config.get("Config", "DebugWithoutGPIO");
+if (config.get("Config", "DebugWithoutGPIO").toLowerCase() === "false") {
+  //pigpio
+  var Gpio = require("pigpio").Gpio;
 
-//red-led
-var led1 = new Gpio(27, {
-	mode: Gpio.OUTPUT
-});
+  //red-led
+  var led1 = new Gpio(parseInt(config.get("Config", "LED_GPIO_PIN_RED")), { mode: Gpio.OUTPUT });
 
-//green-led
-var led2 = new Gpio(22, {
-	mode: Gpio.OUTPUT
-});
+  //green-led
+  var led2 = new Gpio(parseInt(config.get("Config", "LED_GPIO_PIN_GREEN")), { mode: Gpio.OUTPUT });
 
-//blue-led
-var led3 = new Gpio(17, {
-	mode: Gpio.OUTPUT
-});
-
-
-
+  //blue-led
+  var led3 = new Gpio(parseInt(config.get("Config", "LED_GPIO_PIN_BLUE")), { mode: Gpio.OUTPUT });
+}
 //websocket server listen port 3000
 var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({
-	port: wssPort
+  port: wssPort,
 });
 CLIENTS = [];
 
-
 //Webserver objekte
-var express = require('express');
+var express = require("express");
+const { type } = require("express/lib/response");
 var app = express();
-var path = require('path');
+var path = require("path");
+const { exit } = require("process");
+const { application } = require("express");
 
 //----------v webserver v-------------------------
 
@@ -41,69 +56,73 @@ var path = require('path');
 app.use(express.static(__dirname));
 
 //sendet die html datei
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+console.log("Webserver started on port: " + websPort);
+console.log("WebSocketServer started on port: " + wssPort);
 
 //----------v socketserver v-------------------------
 
 //wss = websocketserver selbst
 //ws = websocketserver zu client verbindung
 
-
 // connection = beim connetecten wird die prozedur aufgerufen
 // die individuelle verbindung(session) ist in ws gespeichert
-wss.on('connection', function connection(ws) {
+wss.on("connection", function connection(ws) {
+  //add new connection to list
+  CLIENTS.push(ws);
+  ws.id = getRandomInt(50);
 
-	//add new connection to list
-	CLIENTS.push(ws);
-	ws.id = getRandomInt(50);
+  //serverside send on open:
+  ws.send("Server-Message");
+  console.log("Client joined");
+  console.log("Current clients:" + CLIENTS.length);
+  console.log("Client " + ws.id);
 
+  //serverside recive
+  ws.on("message", function incoming(message) {
+    var obj = JSON.parse(message);
 
-	//serverside send on open:
-	ws.send('Server-Message');
-	console.log("Client joined");
-	console.log("Current clients:" + CLIENTS.length);
-	console.log("Client " + ws.id);
+    //hier werte ausschließen(ist int && nicht <0 und >255)
+    if (Number.isInteger(obj.r) && Number.isInteger(obj.g) && Number.isInteger(obj.b)) {
+      if (obj.r < 256 && obj.g < 256 && obj.b < 256 && obj.r >= 0 && obj.g >= 0 && obj.b >= 0) {
+        if (config.get("Config", "DebugWithoutGPIO").toLowerCase() === "false") {
+          //hier übergabe
+          led1.pwmWrite(obj.r);
+          led2.pwmWrite(obj.g);
+          led3.pwmWrite(obj.b);
+        }
 
-	//serverside recive 
-	ws.on('message', function incoming(message) {
-		var obj = JSON.parse(message);
+        if (GetVerbooseLoggingEnabled()) {
+          console.log("Client " + ws.id + ": " + obj.r + ", " + obj.g + ", " + obj.b);
+        }
+      } else {
+        console.log("Smaller than 0 or bigger than 255");
+      }
+    } else {
+      console.log("Sent package is out of bounds. Only valid RGB values accepted (0-255)");
+    }
+  });
 
-		//hier werte ausschließen(ist int && nicht <0 und >255)
-		if (Number.isInteger(obj.r) && Number.isInteger(obj.g) && Number.isInteger(obj.b)) {
-			if ((obj.r < 256 && obj.g < 256 && obj.b < 256) && (obj.r >= 0 && obj.g >= 0 && obj.b >= 0)) {
-				//hier übergabe 
-				led1.pwmWrite(obj.r);
-				led2.pwmWrite(obj.g);
-				led3.pwmWrite(obj.b);
+  //send on close-connection
+  ws.on("close", function () {
+    ws.close();
+    console.log("closing connection");
+    CLIENTS.pop(ws);
+  });
 
-				// console.log("Client " + ws.id + ": " + obj.r +", "+ obj.g +", "+ obj.b);
-			} else {
-				console.log("Smaller than 0 or bigger than 255")
-			}
-		} else {
-			console.log("Something isn't a integer...feelsbadman")
-		}
-
-	});
-
-	//send on close-connection
-	ws.on('close', function () {
-		ws.close();
-		console.log('closing connection');
-		CLIENTS.pop(ws);
-	});
-
-	ws.on('error', console.error);
+  ws.on("error", console.error);
 });
-
 
 //webserver listen
 app.listen(websPort);
 
-
 function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function GetVerbooseLoggingEnabled() {
+  return config.get("Config", "VerboseLogging").toLowerCase() === "true";
 }
